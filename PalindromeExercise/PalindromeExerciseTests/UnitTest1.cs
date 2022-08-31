6e087b3760a2ce5fcb6aee7f16c0df6a529e96a6
DJ Bates <blooviper@gmail.com>
2022-08-31 08:59:55 -0600

    done

--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using PalindromeExercise;
 using System;
 using Xunit;
 
@@ -5,10 +6,21 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("bob", true)]
+        [InlineData("tree", false)]
+        [InlineData("palindrome", false)]
+        [InlineData("deified", true)]
+        [InlineData("redivider", true)]
+        public void IsPalindromeTest(string value, bool expected)
         {
-
+            //Arrange
+            var test = new WordSmith();
+            //Act
+            var actual = test.IsPalindrome(value);  
+            //Assert
+            Assert.Equal(expected, actual);
         }
     }
 }
